03b1f990ba967350edc02049150685d978500efd
Sreeraj <sreeraj@disroot.org>
2020-05-18 18:13:49 +0530

    initial commit- app barebone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { MovieBody } from './components/movieList.js'
+import InfiniteScroll from 'react-infinite-scroller';
+// import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      movies:
+      [ 
+      ]
+    }
+    this.handleRefresh = this.handleRefresh.bind(this)
+    this.getMovie = this.getMovie.bind(this)
+  }
+
+  handleRefresh() {
+    return new Promise((resolve) => {
+      this.getMovie()
+    });
+  }
+
+  componentDidMount() {
+    this.getMovie()
+  }
+
+  getMovie() {
+    fetch('https://randomuser.me/api/')   //dummy placeholder data for now
+    .then(response => {
+      if(response.ok) return response.json();
+      throw new Error('Request failed.');
+    })
+    .then(data => {
+      console.log(data);
+      this.setState({
+        
+        movies:[
+          ...this.state.movies,
+          {
+            title: data.results[0].name,
+            poster: data.results[0].picture.medium,
+            description: data.results[0].email,
+          },
+        
+        ]
+      });
+      
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <InfiniteScroll
+      pageStart={0} 
+      loadMore={this.handleRefresh}
+      hasMore={true}  // edit with has_more response from api
+      loader={<div className="loader" key={0}>Loading ...</div>}>
+
+      <div className="main-body">
+        {[...this.state.movies].map((movie, index) => {
+          let title = `${movie.title.first} ${movie.title.last}`
+          let  rating= `0.0`
+          let poster = movie.poster
+          let description = movie.description
+          return(
+            <MovieBody 
+              key={index}
+              title={title}
+              rating={rating}
+              description={description}
+              poster={poster} />
+          )
+        })}      
+      </div>
+
+      </InfiniteScroll>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/movieList.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const MovieContainer = (props)=>{
+  return(
+  <div className ='movie-container' >
+      {props.children}
+  </div>)
+}
+
+const MoviePoster = (props)=>{
+  return(
+  <div className ='movie-poster' >
+      <img src={props.poster} alt="Logo" className="picture">
+     </img>
+  </div>)
+}
+
+const MovieDescription = (props)=>{
+  return(
+  <div className ='movie-description' >
+      {props.description}
+  </div>)
+}
+
+const MovieGenres = (props)=>{
+  return(
+  <div className ='movie-genres' >
+
+  </div>)
+}
+
+const MovieTitle = (props)=>{
+  return(
+  <div className ='movie-title' >
+      {props.title}
+  </div>)
+}
+
+const MovieRating = (props)=>{
+  return(
+  <div className ='movie-rating' >
+    {props.rating}
+  </div>)
+}
+
+const MovieBody = (props) => {
+    return(
+      <MovieContainer>
+        <div className="inner-body">
+          <MoviePoster poster={props.poster}/>
+          <div className="body">
+            <div className="inner-body">
+              <MovieTitle title={props.title}/>
+              <MovieRating rating={props.rating}/>
+            </div>
+            <MovieDescription description={props.description}/>
+          </div>
+        </div>
+      </MovieContainer>
+    )
+  }
+  
+  export { MovieBody }
